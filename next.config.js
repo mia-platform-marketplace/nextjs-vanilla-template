90f690ab3b3d3ce847a3c457ced594eb08da2069
Giulio Roggero <giulio.roggero@mia-platform.eu>
2025-12-01 20:48:14 +0100

    removed lint for flow

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
     serverRuntimeConfig: {
       CRUD_PATH: process.env.CRUD_PATH ? process.env.CRUD_PATH : 'REPLACE_SERVER_ENV_CRUD_PATH',
     },
   }
   
   module.exports = nextConfig
-  
