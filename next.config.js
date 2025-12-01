261d33aac49528da0e1d2cdaee1ce6e39d6ac93b
Giulio Roggero <giulio.roggero@mia-platform.eu>
2025-12-01 20:56:09 +0100

    ignore build errors for mia flow test, not prod

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
     serverRuntimeConfig: {
       CRUD_PATH: process.env.CRUD_PATH ? process.env.CRUD_PATH : 'REPLACE_SERVER_ENV_CRUD_PATH',
     },
